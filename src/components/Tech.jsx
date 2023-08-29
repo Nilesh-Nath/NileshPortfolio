08b0973d99837591f6f25f1a44a3ada0ab9f93e4
Nilesh-Nath <nileshnath2060@gmail.com>
2023-08-29 17:41:50 +0545

    tech edit for mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,46 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useState, useEffect } from "react";
 
 const Tech = () => {
-  return (
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for change to the screen size
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+
+    // set the initial value of the isMobile state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return isMobile ? (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-20 h-20 border border-purple-500 rounded-full flex justify-center items-center bg-purple-300">
+          <img
+            width={60}
+            src={technology.icon}
+            alt={technology.name}
+            key={technology.name}
+          />
+        </div>
+      ))}
+    </div>
+  ) : (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
